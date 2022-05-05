1594f015ba9122d54b83aa5e7c1f3a017a17076b
Nikita Kuznetsov <n@8gen.team>
2022-05-05 20:14:59 +0200

    feat: set actual config

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,13 @@ export function getConfig(env: string) {
                 near: {
                     networkId: 'mainnet',
                     nodeUrl: 'https://rpc.mainnet.near.org',
-                    nftContractName: 'nft.exverse.near',
+                    nftContractName: 'battlepass.exverse.near',
                     hallContractName: 'mint.exverse.near',
                     walletUrl: 'https://wallet.near.org',
                     helperUrl: 'https://helper.mainnet.near.org',
                 },
                 eth: {
-                    contract: "0xca55fF22d028656F63E23Af315Adb4e30eD38d60#",
+                    contract: "0x5428a611192c4fce14ef56eff8ee7dab93b8de86",
                 }
             };
         case 'testnet':
@@ -19,8 +19,8 @@ export function getConfig(env: string) {
                 near: {
                     networkId: 'testnet',
                     nodeUrl: 'https://rpc.testnet.near.org',
-                    nftContractName: 'dev-1651263315820-70733957088383',
-                    hallContractName: 'dev-1651263327588-21336933924141',
+                    nftContractName: 'dev-1651761135719-43111996206024',
+                    hallContractName: 'dev-1651761183049-87289918789867',
                     walletUrl: 'https://wallet.testnet.near.org',
                     helperUrl: 'https://helper.testnet.near.org'
                 },
